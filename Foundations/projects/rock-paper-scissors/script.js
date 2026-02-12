69d89b1090ee220bbc3161dfacee79a8c740a55d
Kendrick Lin <kdlin@calpoly.edu>
2026-02-11 21:03:45 -0800

    html file + script and getComputerChoice function implementation

---
+++ Foundations/projects/rock-paper-scissors/script.js
@@ -0,0 +1,15 @@
+console.log("tester") 
+
+function getComputerChoice(){
+    let choice = Math.random()
+    if (choice <= 0.33) {
+        return "rock";
+    } else if (choice > 0.33 && choice <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+console.log(getComputerChoice())
