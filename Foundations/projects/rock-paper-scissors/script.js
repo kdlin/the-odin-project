a85b0160f957cb0a61bd12721ea13b7f6bc99e42
Kendrick Lin <kdlin@calpoly.edu>
2026-02-17 19:40:18 -0800

    finished rock-paper-scissors project

--- Foundations/projects/rock-paper-scissors/script.js
+++ Foundations/projects/rock-paper-scissors/script.js
@@ -1,5 +1,7 @@
-console.log("tester") 
 
+
+
+// Getting Choices 
 function getComputerChoice(){
     let choice = Math.random()
     if (choice <= 0.33) {
@@ -7,31 +9,99 @@ function getComputerChoice(){
     } else if (choice > 0.33 && choice <= 0.66) {
         return "paper";
     } else {
-        return "scissors";
+        return "scissor";
     }
 }
 
 function getPlayerChoice(){
-    let p1_choice = prompt("Enter Rock, Paper, or Scissors: ");
-    return toLowerCase(p1_choice) 
+    let p1_choice = prompt("Enter Rock, Paper, or Scissor: ");
+    return p1_choice.toLowerCase()
 }
+ 
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("Computer Wins");
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You Win!");
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("Computer Wins")
-    } else if (humanChoice == "paper" && computerChoice == "rock") { 
-        console.log("You win!");
-    } else if (humanChoice == "scissor" && computerChoice == "rock") {
-        console.log("Computer Wins");
-    } else {
-        console.log("You win! ")
+
+function entireGame() {
+    let rounds = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (rounds < 5) {
+        console.log(`New round!! Current score Player: ${humanScore} | Computer: ${computerScore}`)
+        playRound(getComputerChoice(), getPlayerChoice())
+
+        if (humanScore > computerScore) {
+        console.log(`Player's score is currently up by ${humanScore-computerScore} | Score: ${humanScore}-${computerScore}`);
+        } else if (humanScore < computerScore) { 
+        console.log(`Currently the computer is winning. Score is: ${computerScore}-${humanScore}`);
+        } else {
+        console.log(`The score is tied ${humanScore}-${computerScore}`);
+      }
+
+       rounds += 1;
     }
-}
-let humanScore = 0;
-let computerScore = 0;
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()))
+    // Score 
+    console.log(`You have finished ${rounds} rounds`);
+    console.log(`Calculating final score...`);
+    setTimeout(() => {
+        console.log('...');
+    }, 2000); 
+    
+    setTimeout(() => {
+        console.log('...');
+    }, 2000); 
+
+    setTimeout(() => {
+        console.log('...');
+    }, 2000); 
+
+    setTimeout(() => {
+        if (humanScore > computerScore) { 
+            console.log(`Player Wins!! Final Score ${humanScore}-${computerScore}`);
+        } else if(humanScore < computerScore) {
+            console.log(`You Lost!! Final Score: ${humanScore}-${computerScore}`);
+        } else {
+            console.log(`It's a Tie. Score: ${humanScore}`);
+        }
+    }, 4000);
+
+    function playRound(humanChoice, compChoice) {
+    if (humanChoice == "rock") {
+        if (compChoice == "rock") {
+            console.log("It's a tie. Both selected rock");
+        } else if (compChoice == "paper") {
+            console.log("Round goes to Computer. +1 points");
+            computerScore += 1;
+        } else {
+            console.log("Player wins. +1 points");
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (compChoice == "paper") { 
+            console.log("It's a tie. Both selected paper");
+        } else if (compChoice == "rock") {
+            console.log("Player wins. +1 points");
+            humanScore += 1;
+        } else {
+            console.log("Round goes to Computer. +1 points");
+            computerScore += 1;
+        }
+    }    
+
+    else{ //humanChoice is scissor
+        if (compChoice == "scissor") {
+            console.log("It's a tie. Both selected scissor");
+        } else if (compChoice == "rock") {
+            console.log("Round goes to Computer. +1 points");
+            computerScore += 1;
+        } else{
+            console.log("Player wins. +1 points");
+            humanScore +=1 ;
+        }
+    }
+    }
+
+}
+
+let result = entireGame()
