427b4486af5e283762d73f5c21972db20be28cb1
Kendrick Lin <kdlin@calpoly.edu>
2026-02-22 17:09:51 -0800

    added reverseString

--- Foundations/javascript-exercises/foundations/07_reverseString/reverseString.js
+++ Foundations/javascript-exercises/foundations/07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
 
-};
-
+const reverseString = function(str) {
+    return [...str].reverse().join("");
+}
 // Do not edit below this line
 module.exports = reverseString;
