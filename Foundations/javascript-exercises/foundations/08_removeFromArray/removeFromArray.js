d9cba0eb074b5bc0c8a433067e0919af37583d95
Kendrick Lin <kdlin@calpoly.edu>
2026-02-22 17:51:14 -0800

    added removeFromArray function

--- Foundations/javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
+++ Foundations/javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...toRemove) {
+   return arr
+    .filter((x) => !toRemove.includes(x))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
