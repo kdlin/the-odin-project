175ff8a0da31d19cd18999bb537c05152c7951d9
Kendrick Lin <kdlin@calpoly.edu>
2026-02-22 20:10:56 -0800

    tempConversion function

--- Foundations/javascript-exercises/foundations/11_tempConversion/tempConversion.js
+++ Foundations/javascript-exercises/foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) { // (x-32) * 5/9
+    return Math.round((fahr - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) { // x * 9/5 + 32 
+  return Math.round((celsius * 9/5 + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
