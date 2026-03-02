fab9c137aae1837d3552f3ba30dc2b28c6821c2e
Kendrick Lin <kdlin@calpoly.edu>
2026-03-01 22:52:29 -0800

    restructured buttons

--- Foundations/projects/rock-paper-scissors/script.js
+++ Foundations/projects/rock-paper-scissors/script.js
@@ -13,69 +13,24 @@ function getComputerChoice(){
     }
 }
 
-function getPlayerChoice(){
-    let p1_choice = prompt("Enter Rock, Paper, or scissors: ");
-    return p1_choice.toLowerCase()
+function playerChoice(selection) { 
+    
 }
- 
 
-const selection1 = document.createElement("button");
-const selection2 = document.createElement("button");
-const selection3 = document.createElement("button");
+const rock = document.querySelector("Rock");
+rock.textContent = "Rock";
+const paper = document.querySelector("Paper");
+paper.textContent = "Paper";
+const scissors = document.querySelector("Scissors"); 
+scissors.textContent = "Scissors";
 
-[selection1, selection2, selection3].forEach(button => {
-    button.addEventListener('click', (e) => {
-        let choice = getPlayerChoice();
+[rock, paper, scissors].forEach(button, (e) => {
+    button.addEventListener('click', () => {
+        playerChoice(button.textContent);
     })
 })
 
-function entireGame() {
-    let rounds = 1;
-    let humanScore = 0;
-    let computerScore = 0;
-
-    while (rounds < 6) {
-        console.log(`Round ${rounds} | Current score Player: ${humanScore} | Computer: ${computerScore}`)
-        playRound(getPlayerChoice(), getComputerChoice());
-
-        if (humanScore > computerScore) {
-        console.log(`Player's score is currently up by ${humanScore-computerScore} | Score: ${humanScore}-${computerScore}`);
-        } else if (humanScore < computerScore) { 
-        console.log(`Currently the computer is winning. Score is: ${computerScore}-${humanScore}`);
-        } else {
-        console.log(`The score is tied ${humanScore}-${computerScore}`);
-      }
-
-       rounds += 1;
-       console.log("-----------------------------------");
-    }
-
-    // Score 
-    console.log(`You have finished ${rounds} rounds`);
-    console.log(`Calculating final score...`);
-    setTimeout(() => {
-        console.log('...');
-    }, 1000); 
-
-    setTimeout(() => {
-        console.log('...');
-    }, 2000); 
-
-    setTimeout(() => {
-        console.log('...');
-    }, 3000); 
-
-    setTimeout(() => {
-        if (humanScore > computerScore) { 
-            console.log(`Player Wins!! Final Score ${humanScore}-${computerScore}`);
-        } else if(humanScore < computerScore) {
-            console.log(`You Lost!! Final Score: ${humanScore}-${computerScore}`);
-        } else {
-            console.log(`It's a Tie. Score: ${humanScore}`);
-        }
-    }, 6000);
-
-    function playRound(humanChoice, compChoice) {
+function playRound(humanChoice, compChoice) {
     if (humanChoice == "rock") {
         if (compChoice == "rock") {
             console.log("It's a tie. Both selected rock");
@@ -112,6 +67,4 @@ function entireGame() {
     }
     }
 
-}
-
-let result = entireGame()
+    
