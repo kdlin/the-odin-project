8bd1854ce527d9c6797e2e5d1424cdb23d83598a
Kendrick Lin <kdlin@calpoly.edu>
2026-03-03 12:50:50 -0800

    added createGrid function and css #container and .grid-square

--- Foundations/projects/etch-a-sketch/script.js
+++ Foundations/projects/etch-a-sketch/script.js
@@ -1,1 +1,11 @@
-const cell = document.createElement("")
+// create grid of any size
+function createGrid(size) {
+    const container = document.querySelector("#container");
+    const totalSquares = size * size; 
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        container.appendChild(square);
+    }
+}
