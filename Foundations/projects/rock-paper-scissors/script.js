01b7530186f0dae82defc58f895578cd7cc408dd
Kendrick Lin <kdlin@calpoly.edu>
2026-03-02 23:24:06 -0800

    re structured html and choicing structure

--- Foundations/projects/rock-paper-scissors/script.js
+++ Foundations/projects/rock-paper-scissors/script.js
@@ -1,70 +1,19 @@
+function playRound(playerChoice) { 
+    let choices = ["Rock", "Paper", "Scissors"];
+    let compChoice = choices[Math.floor(Math.random() * choices.length)]
 
-
-
-// Getting Choices 
-function getComputerChoice(){
-    let choice = Math.random()
-    if (choice <= 0.33) {
-        return "rock";
-    } else if (choice > 0.33 && choice <= 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
+    if (compChoice === playerChoice) { 
+        
     }
+    // insert rest of round syntax here 
 }
 
-function playerChoice(selection) { 
-    
-}
+const Rock = document.querySelector("#Rock");
+const Paper = document.querySelector("#Paper");
+const Scissors = document.querySelector("#Scissors");
 
-const rock = document.querySelector("Rock");
-rock.textContent = "Rock";
-const paper = document.querySelector("Paper");
-paper.textContent = "Paper";
-const scissors = document.querySelector("Scissors"); 
-scissors.textContent = "Scissors";
-
-[rock, paper, scissors].forEach(button, (e) => {
-    button.addEventListener('click', () => {
-        playerChoice(button.textContent);
+[rock, paper, scissors].forEach((selection) => {
+    selection.addEventListener('click', (e) => { 
+        playRound(String(selection));
     })
 })
-
-function playRound(humanChoice, compChoice) {
-    if (humanChoice == "rock") {
-        if (compChoice == "rock") {
-            console.log("It's a tie. Both selected rock");
-        } else if (compChoice == "paper") {
-            console.log("Round goes to Computer. +1 points");
-            computerScore += 1;
-        } else {
-            console.log("Player wins. +1 points");
-            humanScore += 1;
-        }
-    }
-    else if (humanChoice == "paper") {
-        if (compChoice == "paper") { 
-            console.log("It's a tie. Both selected paper");
-        } else if (compChoice == "rock") {
-            console.log("Player wins. +1 points");
-            humanScore += 1;
-        } else {
-            console.log("Round goes to Computer. +1 points");
-            computerScore += 1;
-        }
-    }    
-
-    else{ //humanChoice is scissorss
-        if (compChoice == "scissors") {
-            console.log("It's a tie. Both selected scissors");
-        } else if (compChoice == "rock") {
-            console.log("Round goes to Computer. +1 points");
-            computerScore += 1;
-        } else{
-            console.log("Player wins. +1 points");
-            humanScore +=1 ;
-        }
-    }
-    }
-
-    
