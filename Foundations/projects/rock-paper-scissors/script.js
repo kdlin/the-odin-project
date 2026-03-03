54b361c3151e9dddc63271bc7a946b72844e0d61
Kendrick Lin <kdlin@calpoly.edu>
2026-03-03 00:12:32 -0800

    Game Logic Complete - added playRounds Function - round count logic and end game after reaching 5 points - fixed querySelector bugs

--- Foundations/projects/rock-paper-scissors/script.js
+++ Foundations/projects/rock-paper-scissors/script.js
@@ -1,19 +1,72 @@
+let Status = document.querySelector(".Status");
+let rounds_played = document.querySelector("#NumOfRounds");
+let message = document.querySelector("#Message");
+let scorecard = document.querySelector("#Score");
+
+
+
+let round_counter = 0;
+let playerScore = 0;
+let compScore = 0;
+
 function playRound(playerChoice) { 
+    
+    let round_result;   
     let choices = ["Rock", "Paper", "Scissors"];
     let compChoice = choices[Math.floor(Math.random() * choices.length)]
 
-    if (compChoice === playerChoice) { 
-        
+    if (playerScore === 5 || compScore === 5) {
+        alert("Game Finished");
+        return
     }
-    // insert rest of round syntax here 
+    //round tie 
+    if (compChoice === playerChoice) { 
+        round_result = "Tie";
+    } else if (compChoice === "Rock") { 
+        if (playerChoice === "Paper") {
+            round_result = "Round Win"
+        } else {
+            round_result = "Round Lost"
+      }  
+    } else if (compChoice === "Paper") { 
+        if (playerChoice === "Scissors") {
+            round_result = "Round Win";
+        } else {
+            round_result = "Round Lost";
+        } 
+    } else if (compChoice === "Scissors") { 
+        if (playerChoice === "Rock") { 
+            round_result = "Round Win";
+        } else {
+            round_result = "Round Lost";
+        } 
+    }
+
+    if (round_result === "Round Win") {
+        message.textContent = "Player Wins";
+        playerScore += 1;
+
+    
+    } else if (round_result === "Round Lost") { 
+        message.textContent = "Player Loses"; 
+        compScore += 1;
+    } else { 
+        message.textContent = "Round Tie";
+        // Don't Change either Score 
+    }
+
+    round_counter += 1; 
+    rounds_played.textContent = `Rounds Played: ${round_counter}`; 
+    scorecard.textContent = `Player: ${playerScore} | Computer: ${compScore}`;
+
 }
 
-const Rock = document.querySelector("#Rock");
-const Paper = document.querySelector("#Paper");
-const Scissors = document.querySelector("#Scissors");
+const rock = document.querySelector("#Rock");
+const paper = document.querySelector("#Paper");
+const scissors = document.querySelector("#Scissors");
 
 [rock, paper, scissors].forEach((selection) => {
     selection.addEventListener('click', (e) => { 
-        playRound(String(selection));
+        playRound(selection.textContent);
     })
 })
