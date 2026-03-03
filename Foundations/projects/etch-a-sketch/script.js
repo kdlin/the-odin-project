e711a948f1b5f318f6a649d05503bcfd985e71d9
Kendrick Lin <kdlin@calpoly.edu>
2026-03-03 14:36:32 -0800

    event-listener and createGrid Logic - add resetButton event listener and created reset/clearing of container in createGrid

--- Foundations/projects/etch-a-sketch/script.js
+++ Foundations/projects/etch-a-sketch/script.js
@@ -1,11 +1,37 @@
 // create grid of any size
 function createGrid(size) {
-    const container = document.querySelector("#container");
     const totalSquares = size * size; 
-
-    for (let i = 0; i < totalSquares; i++) {
+    const squareSize = 960 / size;
+    const container = document.querySelector("#container");
+    container.innerHTML = '';
+    document.documentElement.style.setProperty('--square-size', `${squareSize}px`)
+    
+    for (let i = 0; i < totalSquares; i++) { // adding squares to grid
         const square = document.createElement('div');
-        square.classList.add('grid-square');
+        square.classList.add("grid-square");
         container.appendChild(square);
     }
 }
+
+createGrid(64) 
+
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener('click', (e) => {
+    let sizeValid = false;
+
+    while (!sizeValid) {
+        const input = prompt("Please enter new grid size: ");
+        const newSize = parseInt(input);
+
+        if (input === null) {
+            return;
+        }
+        if (Number.isInteger(newSize) && newSize > 0 && newSize <= 100) {
+            sizeValid = true;
+            createGrid(newSize);
+        } else {
+            alert("Please enter a whole number between 1 and 100.")
+        }
+        }
+}); 
