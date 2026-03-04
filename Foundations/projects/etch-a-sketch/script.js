0f38db5187c48960e4ae2ff5beb7b0f1d23330b9
Kendrick Lin <kdlin@calpoly.edu>
2026-03-03 16:22:44 -0800

    syntax consistency code review. Initial gridSize set to 16x16

--- Foundations/projects/etch-a-sketch/script.js
+++ Foundations/projects/etch-a-sketch/script.js
@@ -1,56 +1,56 @@
 const MAX_GRID_SIZE = 100;
-// create grid of any size
+
 function createGrid(size) {
-    const totalSquares = size * size; 
-    const squareSize = 960 / size;
     const container = document.querySelector("#container");
+    document.documentElement.style.setProperty('--square-size', `${960 / size}px`);
     container.innerHTML = '';
-    document.documentElement.style.setProperty('--square-size', `${squareSize}px`)
-    
-    for (let i = 0; i < totalSquares; i++) { // adding squares to grid
+    const totalSize = size * size;
+
+    for (let i = 0; i < totalSize; i++) {
         const square = document.createElement('div');
         square.classList.add("grid-square");
+
         square.addEventListener('mouseover', (e) => {
-            if (square.style.backgroundColor === "") {
+            if (square.style.backgroundColor === '') {
                 const rgbRed = Math.floor(Math.random() * 256);
                 const rgbGreen = Math.floor(Math.random() * 256);
                 const rgbBlue = Math.floor(Math.random() * 256);
 
-                square.style.backgroundColor = `rgb(${rgbRed}, ${rgbGreen}, ${rgbBlue})`
+                square.style.backgroundColor = `rgb(${rgbRed}, ${rgbGreen}, ${rgbBlue})`;
             }
-            
+
             let currOpacity = Number(square.style.opacity);
-            if (currOpacity <= 0.95) {
+            if (currOpacity <= 0.95) { 
                 square.style.opacity = currOpacity + 0.1;
             }
-        })
-       
+        });
+
         container.appendChild(square);
+    }    
+}
+
+const resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click', (e) => {
+    resetGrid(); 
+});
+
+function resetGrid() { 
+    let validSize = false;
+    
+    while (!validSize) {
+        const input = prompt("Please enter new grid size: ");
+        if (input === null) break;
+
+        const newSize = parseInt(input);
+         // finds number 
+        if (Number.isInteger(newSize) && newSize <= MAX_GRID_SIZE && newSize > 0) {
+            validSize = true;
+            createGrid(newSize);
+        } else {
+            alert("Enter Whole number between 1-100.");
+        }
     }
 }
 
-createGrid(64) 
-
-const resetButton = document.querySelector("#reset");
-
-resetButton.addEventListener('click', (e) => {
-    let sizeValid = false;
-
-    while (!sizeValid) {
-        const input = prompt("Please enter new grid size: ");
-        if (input === null) {
-            break; }
-
-        const newSize = parseInt(input);
-
-        if (Number.isInteger(newSize) && newSize > 0 && newSize <= MAX_GRID_SIZE) {
-            sizeValid = true;
-            createGrid(newSize);
-        } else {
-            alert("Please enter a whole number between 1 and 100.")
-        }
-        }
-}); 
-
-
-
+createGrid(16);
