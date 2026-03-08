2af01bc4bcdde184908ced5431ca5ae085618074
Kendrick Lin <kdlin@calpoly.edu>
2026-03-07 18:51:51 -0800

    finished fibonacci function

--- Foundations/javascript-exercises/foundations/14_fibonacci/fibonacci.js
+++ Foundations/javascript-exercises/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
 
+    let count = 0;
+    let prev = 0;
+    let fibo = 1;
+    while (count < num - 1) {
+         [prev, fibo] = [fibo, fibo + prev]
+         count ++;
+    }
+    return fibo; 
 };
 
 // Do not edit below this line
