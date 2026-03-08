d193434f8156e32cedfa6a01ccde1d4bbb315fe2
Kendrick Lin <kdlin@calpoly.edu>
2026-03-07 19:12:47 -0800

    finished findTheOldest and tests

--- Foundations/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ Foundations/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let descendingAges = arr.sort((p1, p2) => { 
+        let currYear = (new Date()).getFullYear();
+        let p1Age = (p1.yearOfDeath || currYear) - p1.yearOfBirth;
+        let p2Age = (p2.yearOfDeath || currYear) - p2.yearOfBirth;
+        
+        return p2Age - p1Age;
+    } )
 
+    return descendingAges[0];
 };
 
 // Do not edit below this line
