5f3ed338c7e6f2f6f9ac844a0d09435efc373e2c
Kendrick Lin <kdlin@calpoly.edu>
2026-03-07 17:55:16 -0800

    palindromes function

--- Foundations/javascript-exercises/foundations/13_palindromes/palindromes.js
+++ Foundations/javascript-exercises/foundations/13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let left = 0;
+    let right = str.length - 1;
 
+    let str = str
+    .filter(char => /[a-z0-9]/i.test(char))
+    .join('')
+    .toLowerCase();
+
+
+    // two pointer approach 
+    while (right > left) { 
+        if (str[left] !== str[right]) {
+            return false;
+        }
+
+        left ++;
+        right --;
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
