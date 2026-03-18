f2d9b4e40b4f7485585bc8a027e2d0d5902fb2e4
Kendrick Lin <kdlin@calpoly.edu>
2026-03-18 12:27:17 -0700

    Initial file structure and boilerplate

--- Foundations/javascript-exercises/foundations/13_palindromes/palindromes.js
+++ Foundations/javascript-exercises/foundations/13_palindromes/palindromes.js
@@ -1,25 +1,13 @@
-const palindromes = function (str) {
-    let left = 0;
-    let right = str.length - 1;
+const palindromes = function (str) { 
+    let cleaned = str
+        .split('')
+        .filter(char => /[a-z0-9]/i.test(char))
+        .join('')
+        .toLowerCase();
 
-    let str = str
-    .filter(char => /[a-z0-9]/i.test(char))
-    .join('')
-    .toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
 
+    return cleaned === reversed;
+  };
 
-    // two pointer approach 
-    while (right > left) { 
-        if (str[left] !== str[right]) {
-            return false;
-        }
-
-        left ++;
-        right --;
-    }
-    
-    return true;
-};
-
-// Do not edit below this line
 module.exports = palindromes;
