cfeba3a9b6257540064b6f95bcab28409de8954c
Kendrick Lin <kdlin@calpoly.edu>
2026-03-30 14:22:50 -0700

    added operate function, event listenrs for [nums, operations, clear, equal], and display feature

--- Foundations/projects/calculator/script.js
+++ Foundations/projects/calculator/script.js
@@ -1,9 +1,75 @@
 let resultDisplayed = false;
-let num1 = null;
-let num2 = null;
-let operator = null;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let display = document.querySelector(".display")
+let digits = document.querySelectorAll(".digits button")
+let operators = document.querySelectorAll(".operators button")
+let clear = document.querySelector("#clear")
+let equal = document.querySelector("#equal")
 
-let digits = document.querySelectorAll(".digits")
+clear.addEventListener("click", () => {
+    // Clear the display and vals
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    resultDisplayed = false;
+    operator = '';
+})
+
+equal.addEventListener("click", () => {
+    if (num1 === "" || num2 === "" || operator === "") {
+        alert("Please input 2 numbers and the operator. Clear First");
+        return;
+    }
+    display.textContent = operate(operator, parseFloat(num1), parseFloat(num2));
+
+})
+
+digits.forEach((btn) => {
+    btn.addEventListener ("click", () => {
+        if (resultDisplayed === false && operator === '') {
+            num1 = num1 + btn.textContent;
+            display.textContent = num1 + operator + num2; 
+        } else if (resultDisplayed === false && operator !== '') {
+            num2 = num2 + btn.textContent;
+            display.textContent = num1 + operator + num2; 
+        } else if (resultDisplayed === true){
+            num1 = "";
+            num2 = "" ;
+            resultDisplayed = false;
+            operator = ""; 
+            num1 = btn.textContent;
+            display.textContent = num1;
+        }   
+    })
+})
+
+operators.forEach((btn) => { 
+    btn.addEventListener ("click", () => { 
+        if (resultDisplayed === false) { 
+            if (btn.textContent === "+") {
+                operator = "+";
+                display.textContent = num1 + operator + num2;
+            } 
+            else if (btn.textContent === "-") {
+                operator = '-';
+                display.textContent = num1 + operator + num2;
+            }
+            else if (btn.textContent === "/") {
+                operator  = "/";
+                display.textContent = num1 + operator + num2;
+            } else if (btn.textContent === "*") {
+                operator = "*";
+                display.textContent = num1 + operator + num2;
+            }
+        }
+    })
+})
+
+
+
+
 
 function add(a, b) { 
     return a + b;
@@ -13,7 +79,7 @@ function subtract(a, b) {
     return a - b;
 }
 
-function divide(a, b) {
+function divide(a, b) { 
     if (b === 0) {
         return "Cannot divide by 0 dumbass"
     }
